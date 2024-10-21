eaeba1b209831517012dca7c35daf1e864e49a7a
rafaelanacleto <rafael.ferreira.anacleto@gmail.com>
2024-10-21 09:36:54 -0300

    Adcionado paste no input file

---
+++ Gestao/Gestao.Client/Pages/ImagePasteComponent.razor.js
@@ -0,0 +1,25 @@
+﻿window.ImagePasteComponent = {
+    init: function (inputFile) {
+        document.addEventListener('paste', function (e) {
+            if (e.clipboardData) {
+                var items = e.clipboardData.items;
+                for (var i = 0; i < items.length; i++) {
+                    if (items[i].kind === 'file' && items[i].type.indexOf('image') !== -1) {
+                        var blob = items[i].getAsFile();
+                        var url = URL.createObjectURL(blob);
+                        document.getElementById('pastedImage').src = url;
+                        console.log(url);
+                        var input = inputFile;
+                        var dt = new DataTransfer();
+                        dt.items.add(blob);
+                        input.files = dt.files;
+                    }
+                }
+            }
+        });
+    },
+    setImage: function (imageSrc) {
+        document.getElementById('pastedImage').src = imageSrc;
+        console.log(imageSrc);
+    }
+};
