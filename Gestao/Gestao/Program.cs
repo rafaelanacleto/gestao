60f0b429b70260ac5c971d20442e998d6e6df963
rafaelanacleto <rafael.ferreira.anacleto@gmail.com>
2024-11-28 11:02:28 -0300

    Ajuste appsettings banco de dados

--- Gestao/Gestao/Program.cs
+++ Gestao/Gestao/Program.cs
@@ -61,33 +61,6 @@ namespace Gestao
             });
             builder.Services.AddSingleton<IEmailSender<ApplicationUser>, EmailSender>();
             
-            builder.Services.AddAuthentication()
-            .AddGoogle(options =>
-            {
-                IConfigurationSection googleAuthNSection =
-                config.GetSection("Authentication:Google");
-                options.ClientId = googleAuthNSection["ClientId"];
-                options.ClientSecret = googleAuthNSection["ClientSecret"];
-            })
-            .AddFacebook(options =>
-            {
-                IConfigurationSection FBAuthNSection =
-                config.GetSection("Authentication:FB");
-                options.ClientId = FBAuthNSection["ClientId"];
-                options.ClientSecret = FBAuthNSection["ClientSecret"];
-            })
-            .AddMicrosoftAccount(microsoftOptions =>
-            {
-                microsoftOptions.ClientId = config["Authentication:Microsoft:ClientId"];
-                microsoftOptions.ClientSecret = config["Authentication:Microsoft:ClientSecret"];
-            })
-            .AddTwitter(twitterOptions =>
-            {
-                twitterOptions.ConsumerKey = config["Authentication:Twitter:ConsumerAPIKey"];
-                twitterOptions.ConsumerSecret = config["Authentication:Twitter:ConsumerSecret"];
-                twitterOptions.RetrieveUserDetails = true;
-            });
-
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
