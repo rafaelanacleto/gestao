d8f2c2533ed2790714ce638f4b36cafe635966ff
rafaelanacleto <rafael.ferreira.anacleto@gmail.com>
2024-12-03 08:52:58 -0300

    TEstar no windows

--- Gestao/Gestao/Program.cs
+++ Gestao/Gestao/Program.cs
@@ -32,7 +32,14 @@ namespace Gestao
                     options.DefaultScheme = IdentityConstants.ApplicationScheme;
                     options.DefaultSignInScheme = IdentityConstants.ExternalScheme;
                 })
-                .AddIdentityCookies();
+                .AddIdentityCookies()
+                .AddGoogle(options =>
+                {
+                    IConfigurationSection googleAuthNSection =
+                    config.GetSection("Authentication:Google");
+                    options.ClientId = googleAuthNSection["ClientId"];
+                    options.ClientSecret = googleAuthNSection["ClientSecret"];
+                });
 
             var connectionString = builder.Configuration.GetConnectionString("DefaultConnectionDocker") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
             builder.Services.AddDbContext<ApplicationDbContext>(options =>
